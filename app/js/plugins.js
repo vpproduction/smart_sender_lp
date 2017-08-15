15c224550f2cb59e6c309d320de19ebcb2a5cb39
AVorona <vitalii.parshikov@gmail.com>
2017-08-16 00:26:18 +0300

    initialize

---
+++ app/js/plugins.js
@@ -0,0 +1,43 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+
+$('.slider').slick({
+  centerMode: true,
+  centerPadding: '16%',
+  slidesToShow: 1,
+   arrows: false,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '8%',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
